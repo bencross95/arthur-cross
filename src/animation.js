ed854c3a3b74e6186f9b2136cf1b94882f8ad054
Ben Cross <ben_cross@me.com>
2023-12-17 23:04:03 +0100

    attemped anim

--- src/animation.js
+++ src/animation.js
@@ -2,4 +2,14 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 
 
 
-gsap.fromTo("body", { opacity: 0 }, { opacity: 1, duration: 1, delay: 0.2 });
+// gsap.fromTo("body", { opacity: 0 }, { opacity: 1, duration: 1, delay: 0.2 });
+
+
+var tl = gsap.timeline();
+
+tl.to(".projectItemList", {
+    keyframes: {
+      opacity: [0, 1],
+    },
+    duration: 0.5, // 0.1 seconds between when each ".box" element starts animating
+  });
