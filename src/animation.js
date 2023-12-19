3e24447db60291445946ed74dcaeb1c59c6059e8
Ben Cross <ben_cross@me.com>
2023-12-19 15:55:45 +0100

    more wack 3D

--- src/animation.js
+++ src/animation.js
@@ -1,25 +1,19 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 
-
-
 gsap.fromTo("body", { opacity: 0 }, { opacity: 1, duration: 1, delay: 0.2 });
-
 
 var tl = gsap.timeline();
 
-// tl.to(".projectItemList", {
-//     keyframes: {
-//       opacity: [0, 1],
-//     },
-//     duration: 0.8,
-//     stagger: 0.05, // 0.1 seconds between when each ".box" element starts animating
-//   });
-
-
 tl.to(".projectItemList", {
-    keyframes: {
-      opacity: [0, 1],
-    },
-    duration: 0.8,
-    stagger: 0.07, // 0.1 seconds between when each ".box" element starts animating
-  });
+  keyframes: {
+    opacity: [0, 1],
+  },
+  duration: 0.8,
+  stagger: 0.07, // 0.1 seconds between when each ".box" element starts animating
+});
+
+gsap.fromTo(
+  ".arthur3d-div",
+  { opacity: 0 },
+  { opacity: 1, duration: 8, delay: 3 }
+);
